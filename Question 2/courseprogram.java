dbeb45bee6b95ee5608c13ec090fdb70e889037c
shirishdwivedi27 <shirishdivedi951@gmail.com>
2025-06-07 11:37:25 +0530

    initial java

--- Question 2/courseprogram.java
+++ Question 2/courseprogram.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class courseProgram {
+public class courseProgram{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Course[] course = new Course[4];
