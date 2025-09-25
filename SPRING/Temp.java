7e5071952ff66ed9b16993a65a95749565d96155
shirishdwivedi27 <shirishdivedi951@gmail.com>
2025-09-25 10:41:09 +0530

    DI and IOC PRACTISE

---
+++ SPRING/Temp.java
@@ -0,0 +1,12 @@
+/*
+ * Payment Gateway Example
+ * Define an interface PaymentProcessor with processPayment(double amount).
+ * 
+ * Implement two processors: CreditCardProcessor and UPIProcessor.
+ * 
+ * Inject the processor into an OrderService class.
+ * 
+ * Demonstrate IoC by switching processors without modifying OrderService.
+ * 
+ * 
+ */
