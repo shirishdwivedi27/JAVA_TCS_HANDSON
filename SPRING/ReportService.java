5ea0a36f2526782297f79ff264ba2840e9d34556
shirishdwivedi27 <shirishdivedi951@gmail.com>
2025-09-25 10:29:42 +0530

    start spring

---
+++ SPRING/ReportService.java
@@ -0,0 +1,13 @@
+/*
+ * Printer Example
+Create an interface Printer with a method print(String message).
+
+Implement two classes: ConsolePrinter and FilePrinter.
+
+Use dependency injection so that a ReportService can use either printer without changing its code.
+
+ */
+
+public class ReportService {
+    // Service methods for handling reports
+}
