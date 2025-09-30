93d91e86a04be27cf00079dacaa1f0a534cd09bb
shirishdwivedi27 <shirishdivedi951@gmail.com>
2025-09-30 23:39:40 +0530

    DI and IOC PRACTISE

--- SPRING/Temp.java
+++ SPRING/Temp.java
@@ -10,3 +10,45 @@
  * 
  * 
  */
+
+interface PaymentProcessor {
+    void processPayment(double amount);
+}
+
+class CreditCardProcessor implements PaymentProcessor {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing credit card payment of $" + amount);
+    }
+}
+
+class UPIProcessor implements PaymentProcessor {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing UPI payment of $" + amount);
+    }
+}
+
+class OrderService {
+    private PaymentProcessor paymentProcessor;
+
+    public OrderService(PaymentProcessor paymentProcessor) {
+        this.paymentProcessor = paymentProcessor;
+    }
+
+    public void placeOrder(double amount) {
+        System.out.println("Placing order of $" + amount);
+        paymentProcessor.processPayment(amount);
+    }
+}
+
+public class Temp {
+    public static void main(String[] args) {
+
+        OrderService orderService1 = new OrderService(new CreditCardProcessor());
+        orderService1.placeOrder(100.0);
+
+        OrderService orderService2 = new OrderService(new UPIProcessor());
+        orderService2.placeOrder(200.0);
+    }
+}
